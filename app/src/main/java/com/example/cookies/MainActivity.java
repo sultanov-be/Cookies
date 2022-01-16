a3c12df6157f47b14a8ce8527aa02476f1fdbe6a
sultanov-be <sultanov_b@auca.kg>
2022-01-16 13:41:42 +0600

    Initial commit

---
+++ app/src/main/java/com/example/cookies/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.cookies;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
+
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
+    }
+}
